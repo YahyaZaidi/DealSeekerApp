88aa8ae6beae1f14101632eec42f9cdbebc10a51
YahyaZaidi <cbm2522@autuni.ac.nz>
2024-05-12 23:06:23 +1200

    commit-yahya
    Removing buttons from home fragment

--- app/src/main/java/com/example/dealseekerapplication/PhonesAndGPS.kt
+++ app/src/main/java/com/example/dealseekerapplication/PhonesAndGPS.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [PhonesAndGPS.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PhonesAndGPS : Fragment(R.layout.fragment_phones_and_g_p_s) {
+class PhonesAndGPS : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
