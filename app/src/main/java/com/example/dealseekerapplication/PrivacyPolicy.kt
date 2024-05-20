4d4395634e2f6e7987014b6fac929c0b01829b40
J4yRodz <50560614+J4yRodz@users.noreply.github.com>
2024-05-20 18:30:48 +1200

    added privacy policy page, no button functionality yet.

--- app/src/main/java/com/example/dealseekerapplication/PrivacyPolicy.kt
+++ app/src/main/java/com/example/dealseekerapplication/PrivacyPolicy.kt
@@ -13,19 +13,15 @@ private const val ARG_PARAM2 = "param2"
 
 
 
-
 /**
  * A simple [Fragment] subclass.
- * Use the [Profile.newInstance] factory method to
+ * Use the [PrivacyPolicu.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Profile : Fragment() {
+class PrivacyPolicy : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +36,7 @@ class Profile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_privacypolicy, container, false)
     }
 
     companion object {
@@ -55,7 +51,7 @@ class Profile : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Profile().apply {
+            PrivacyPolicy().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
