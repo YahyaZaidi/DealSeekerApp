0b33c1eee973eab92c8af4458f9f81c81bbdb25d
KingzCall <malachinata@gmail.com>
2024-05-24 00:31:37 +1200

    Adding dependencies
    Dependencies necessary for implementing API calls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,12 @@ android {
 
 dependencies {
 
+    //api dependencies
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation(libs.androidx.core.ktx)
