466c2364e17ffac6ff9eae62ba9a3ebc25571c81
KingzCall <malachinata@gmail.com>
2024-06-04 19:41:52 +1200

    Adding dependencies
    Adding firestore database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     //firebase dependencies
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore:24.0.0")
+
 
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
